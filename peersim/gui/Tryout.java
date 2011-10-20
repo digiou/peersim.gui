07cd2b2017a895fe32971067943e01b80e263c3c
Dimitrios <cst06039@uop.gr>
2011-10-21 00:48:38 +0300

    working arrows, subclassing them does not worked so far

--- peersim/gui/Tryout.java
+++ peersim/gui/Tryout.java
@@ -1,6 +1,6 @@
 package peersim.gui;
 
-//piccolo2d imports
+//Piccolo2d imports
 import java.awt.Dimension;
 import java.awt.Color;
 import java.awt.event.InputEvent;
@@ -16,6 +16,8 @@ import edu.umd.cs.piccolo.event.PInputEventFilter;
 import edu.umd.cs.piccolo.nodes.PPath;
 import edu.umd.cs.piccolox.PFrame;
 import edu.umd.cs.piccolox.pswing.PSwingCanvas;
+import java.awt.Shape;
+import java.awt.geom.GeneralPath;
 
 //peersim imports
 import peersim.core.Network;
@@ -34,8 +36,8 @@ public class Tryout extends PFrame {
     private static final Dimension SCREEN = new Dimension(1024, 768);
     private static final int NODES = Network.size();
     private PCamera camera;
-    
-    public Tryout(){
+
+    public Tryout() {
         super("Tryout", false, new PSwingCanvas());
     }
 
@@ -69,9 +71,14 @@ public class Tryout extends PFrame {
 
                 PNode node1 = nodeLayer.getChild(n1);
                 PNode node2 = nodeLayer.getChild(n2);
-                PPath edge = new PPath();
+                Point2D start = node1.getFullBoundsReference().getCenter2D();
+                Point2D end = node2.getFullBoundsReference().getCenter2D();
+                PPath edge = new PPath(createArrow(new Point2D.Double(start.getX(), start.getY()),
+                        new Point2D.Double(end.getX(), end.getY())));
+
+
                 ((ArrayList) node1.getAttribute("edges")).add(edge);
-                ((ArrayList) node1.getAttribute("nextPeers")).add(node2); 
+                ((ArrayList) node1.getAttribute("nextPeers")).add(node2);
                 ((ArrayList) node2.getAttribute("edges")).add(edge);
                 ((ArrayList) node2.getAttribute("prevPeers")).add(node1);
                 edge.addAttribute("nodes", new ArrayList());
@@ -96,11 +103,11 @@ public class Tryout extends PFrame {
                     e.getPickedNode().setPaint(Color.RED);
                     ArrayList prevPeers = (ArrayList) e.getPickedNode().getAttribute("prevPeers");
                     ArrayList nextPeers = (ArrayList) e.getPickedNode().getAttribute("nextPeers");
-                    for(int i = 0; i<prevPeers.size();i++){
+                    for (int i = 0; i < prevPeers.size(); i++) {
                         PNode prevNode = (PNode) prevPeers.get(i);
                         prevNode.setPaint(Color.GREEN);
                     }
-                    for(int i = 0; i<nextPeers.size();i++){
+                    for (int i = 0; i < nextPeers.size(); i++) {
                         PNode nextNode = (PNode) nextPeers.get(i);
                         nextNode.setPaint(Color.BLUE);
                     }
@@ -113,11 +120,11 @@ public class Tryout extends PFrame {
                     e.getPickedNode().setPaint(Color.WHITE);
                     ArrayList prevPeers = (ArrayList) e.getPickedNode().getAttribute("prevPeers");
                     ArrayList nextPeers = (ArrayList) e.getPickedNode().getAttribute("nextPeers");
-                    for(int i = 0; i<prevPeers.size();i++){
+                    for (int i = 0; i < prevPeers.size(); i++) {
                         PNode prevNode = (PNode) prevPeers.get(i);
                         prevNode.setPaint(Color.WHITE);
                     }
-                    for(int i = 0; i<nextPeers.size();i++){
+                    for (int i = 0; i < nextPeers.size(); i++) {
                         PNode nextNode = (PNode) nextPeers.get(i);
                         nextNode.setPaint(Color.WHITE);
                     }
@@ -142,13 +149,43 @@ public class Tryout extends PFrame {
 
     }
 
+    private Shape createArrow(Point2D start, Point2D end) {
+
+        // Arrow settings.
+        int b = 20;
+        double theta = Math.toRadians(17);
+
+        // Arrow Calculations.
+        double xs = start.getX();
+        double ys = start.getY();
+        double xe = end.getX();
+        double ye = end.getY();
+        double alpha = Math.atan2(ye - ys, xe - xs);
+        double dx1 = b * Math.cos(alpha + theta);
+        double dy1 = b * Math.sin(alpha + theta);
+        double dx2 = b * Math.cos(alpha - theta);
+        double dy2 = b * Math.sin(alpha - theta);
+
+        // Arrow Path.
+        GeneralPath path = new GeneralPath();
+        path.moveTo(xs, ys);
+        path.lineTo(xe, ye);
+        path.lineTo(xe - dx1, ye - dy1);
+        path.moveTo(xe, ye);
+        path.lineTo(xe - dx2, ye - dy2);
+
+        return path;
+    }
+
     public void updateEdge(PPath edge) {
         PNode node1 = (PNode) ((ArrayList) edge.getAttribute("nodes")).get(0);
         PNode node2 = (PNode) ((ArrayList) edge.getAttribute("nodes")).get(1);
         Point2D start = node1.getFullBoundsReference().getCenter2D();
         Point2D end = node2.getFullBoundsReference().getCenter2D();
-        edge.reset();
-        edge.moveTo((float) start.getX(), (float) start.getY());
-        edge.lineTo((float) end.getX(), (float) end.getY());
+
+        final Shape newArrow = createArrow(
+                new Point2D.Double(start.getX(), start.getY()), new Point2D.Double(end.getX(),
+                end.getY()));
+        edge.setPathTo(newArrow);
     }
 }
