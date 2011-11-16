3d55c7aa7a139530800e7ca5c2f4b1d90ae506d8
digiou <cst06039@uop.gr>
2011-11-16 21:20:25 +0200

    16-10-2011

--- peersim/gui/StartGui.java
+++ peersim/gui/StartGui.java
@@ -4,6 +4,7 @@
  */
 package peersim.gui;
 
+import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
@@ -12,14 +13,20 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Formatter;
 import java.util.Scanner;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
+import javax.swing.JTabbedPane;
 import javax.swing.JTextArea;
+import javax.swing.UIManager;
+import javax.swing.border.EtchedBorder;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 
@@ -29,24 +36,46 @@ import javax.swing.event.DocumentListener;
  */
 public class StartGui extends JFrame {
 
-    private JTextArea textArea = new JTextArea();
+    private JTextArea inputTextArea = new JTextArea();
+    private JTextArea outputTextArea = new JTextArea();
+    private JLabel status;
     private File file;
     private JFileChooser fileChooser = new JFileChooser();
     private boolean isSaved = true;
     DetectChangeDocumentListener textAreaDocumentListener;
-    private Object aboutDialog;
+    
+    public static void main(String[] args) {
+        try{
+            UIManager.setLookAndFeel(
+            UIManager.getSystemLookAndFeelClassName());
+        }catch(Exception e){
+            System.err.println("Determining system L&F failed: falling back to default");
+        }
+        
+        StartGui myGui = new StartGui();
+        myGui.setVisible(true);
+    }
 
     public StartGui() {
         super("PeerSim GUI");
-        setSize(1024, 768);
+        setSize(800, 600);
         setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        setLayout(new BorderLayout());
+                
         textAreaDocumentListener = new DetectChangeDocumentListener(this);
-        textArea.getDocument().addDocumentListener(textAreaDocumentListener);
-        JScrollPane scrollPane = new JScrollPane(textArea);
-        add(scrollPane);
+        inputTextArea.getDocument().addDocumentListener(textAreaDocumentListener);
+        JScrollPane inputScrollPane = new JScrollPane(inputTextArea);
+        outputTextArea.setEditable(false);
+        JScrollPane outputScrollPane = new JScrollPane(outputTextArea);
+        
+        JTabbedPane tabbedPane = new JTabbedPane();
+        tabbedPane.addTab("Input file", inputScrollPane);
+        tabbedPane.addTab("Console output", outputScrollPane);
+        tabbedPane.setTabPlacement(JTabbedPane.BOTTOM);
+        add(tabbedPane, BorderLayout.CENTER);
 
-        JMenu fileMenu = new JMenu("Αρχείο");
-        JMenuItem openMenuItem = new JMenuItem("Άνοιγμα");
+        JMenu fileMenu = new JMenu("File");
+        JMenuItem openMenuItem = new JMenuItem("Open file");
         openMenuItem.addActionListener(new ActionListener() {
 
             public void actionPerformed(ActionEvent e) {
@@ -54,7 +83,7 @@ public class StartGui extends JFrame {
             }
         });
 
-        JMenuItem saveMenuItem = new JMenuItem("Αποθήκευση");
+        JMenuItem saveMenuItem = new JMenuItem("Save file");
         saveMenuItem.addActionListener(new ActionListener() {
 
             public void actionPerformed(ActionEvent e) {
@@ -62,7 +91,7 @@ public class StartGui extends JFrame {
             }
         });
 
-        JMenuItem exitMenuItem = new JMenuItem("Έξοδος");
+        JMenuItem exitMenuItem = new JMenuItem("Exit");
         exitMenuItem.addActionListener(new ActionListener() {
 
             public void actionPerformed(ActionEvent e) {
@@ -74,15 +103,27 @@ public class StartGui extends JFrame {
         fileMenu.add(saveMenuItem);
         fileMenu.addSeparator();
         fileMenu.add(exitMenuItem);
-
-        JMenu helpMenu = new JMenu("Βοήθεια");
+        
+        JMenu simuMenu = new JMenu("Simulation");
+        JMenuItem startMenuItem = new JMenuItem("Start");
+        startMenuItem.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                startSimuation();
+            }
+        });
+        
+        simuMenu.add(startMenuItem);
 
         JMenuBar menuBar = new JMenuBar();
         menuBar.add(fileMenu);
-        menuBar.add(helpMenu);
+        menuBar.add(simuMenu);
 
         setJMenuBar(menuBar);
-
+        
+        status = new JLabel("No file selected.");
+        status.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.RAISED));
+        add(status, BorderLayout.PAGE_END);
+        
         addWindowListener(new WindowAdapter() {
 
             public void windowClosing(WindowEvent e) {
@@ -102,9 +143,11 @@ public class StartGui extends JFrame {
                 while (fileReader.hasNextLine()) {
                     textBuffer.append(fileReader.nextLine() + "\n");
                 }
-                textArea.setText(textBuffer.toString());
+                inputTextArea.setText(textBuffer.toString());
+                inputTextArea.setEditable(true);
+                status.setText("Using file: "+ file.getAbsolutePath());
             } catch (FileNotFoundException e) {
-                JOptionPane.showMessageDialog(this, "Δεν είναι δυνατό το άνοιγμα του αρχείου.", "Σφάλμα", JOptionPane.ERROR_MESSAGE);
+                JOptionPane.showMessageDialog(this, "Couldn't save file.", "Error", JOptionPane.ERROR_MESSAGE);
                 return;
             }
         }
@@ -119,7 +162,7 @@ public class StartGui extends JFrame {
     }
 
     private void askToSaveFile() {
-        int result = JOptionPane.showConfirmDialog(this, "Το κείμενο έχει τροποποιηθεί.\n\nΝα γίνει αποθήκευση;", "Προειδοποίηση", JOptionPane.YES_NO_CANCEL_OPTION);
+        int result = JOptionPane.showConfirmDialog(this, "The text has been modified.\nSave?", "Warning", JOptionPane.YES_NO_CANCEL_OPTION);
         if (result == JOptionPane.YES_OPTION) {
             if (saveFile()) {
                 dispose();
@@ -140,19 +183,23 @@ public class StartGui extends JFrame {
         if (result == JFileChooser.APPROVE_OPTION) {
             try {
                 Formatter fileWriter = new Formatter(fileChooser.getSelectedFile());
-                fileWriter.format("%s", textArea.getText());
+                fileWriter.format("%s", inputTextArea.getText());
                 fileWriter.close();
                 file = fileChooser.getSelectedFile();
                 isSaved = true;
-                textArea.getDocument().addDocumentListener(textAreaDocumentListener);
+                inputTextArea.getDocument().addDocumentListener(textAreaDocumentListener);
                 return true;
             } catch (FileNotFoundException e) {
-                JOptionPane.showMessageDialog(this, "Δεν είναι δυνατή η αποθήκευση του αρχείου.", "Σφάλμα", JOptionPane.ERROR_MESSAGE);
+                JOptionPane.showMessageDialog(this, "Couldn't save file!", "Error", JOptionPane.ERROR_MESSAGE);
                 return false;
             }
         }
 
         return false;
+    }
+    
+    private void startSimuation(){
+        
     }
 
     private class DetectChangeDocumentListener implements DocumentListener {
@@ -176,8 +223,11 @@ public class StartGui extends JFrame {
         }
 
         private void markChanged() {
-            /*editor.*/ isSaved = false;
-            /*editor.*/ textArea.getDocument().removeDocumentListener(this);
+            isSaved = false;
+            if(status.getText().contains("No file selected.")){
+                status.setText("Using new file.");
+            }
+            inputTextArea.getDocument().removeDocumentListener(this);
         }
     }
 }
