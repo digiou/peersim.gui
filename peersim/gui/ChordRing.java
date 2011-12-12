358f3af82649a2166b4b4a13aaf74a3a48ec1104
digiou <cst06039@uop.gr>
2011-12-12 02:01:36 +0200

    12-12-2011

--- peersim/gui/ChordRing.java
+++ peersim/gui/ChordRing.java
@@ -14,10 +14,12 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Shape;
 import java.awt.event.InputEvent;
+import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.geom.GeneralPath;
 import java.awt.geom.Point2D;
 import java.util.ArrayList;
+import java.util.Hashtable;
 import peersim.chord.ChordProtocol;
 import peersim.core.Linkable;
 import peersim.core.Network;
@@ -37,6 +39,7 @@ public class ChordRing extends PFrame {
     int radius = width / 2;
     float cx = margin + radius;
     float cy = margin + radius;
+    Hashtable<Long, Integer> hashTable = new Hashtable<Long, Integer>(Network.size());
 
     public static void main(String[] args) {
         new ChordRing();
@@ -51,48 +54,49 @@ public class ChordRing extends PFrame {
         setSize(SCREEN);
         final PCanvas canvas = getCanvas();
         camera = canvas.getCamera();
-        PLayer nodeLayer = canvas.getLayer();
+        final PLayer nodeLayer = canvas.getLayer();
         PLayer edgeLayer = new PLayer();
         canvas.getRoot().addChild(edgeLayer);
         camera.addLayer(0, edgeLayer);
 
-        Point2D center = new Point2D.Double((float) (SCREEN.width / 2), (float) SCREEN.height);
+        drawNodes(nodeLayer);
+        /*Point2D center = new Point2D.Double((float) (SCREEN.width / 2), (float) SCREEN.height);
         ArrayList<Point2D> circlePoints = circlePoints(center, (float) SCREEN.height / 2);
-
+        
         for (int i = 0;i < NODES;i++) {
-            Point2D point = circlePoints.get(i);
-            PPath node = PPath.createEllipse((float) point.getX(), (float) point.getY(), 16, 16);
-            node.addAttribute("edges", new ArrayList());
-            node.addAttribute("prevPeers", new ArrayList());
-            node.addAttribute("nextPeers", new ArrayList());
-            nodeLayer.addChild(node);
+        Point2D point = circlePoints.get(i);
+        PPath node = PPath.createEllipse((float) point.getX(), (float) point.getY(), 16, 16);
+        node.addAttribute("edges", new ArrayList());
+        node.addAttribute("prevPeers", new ArrayList());
+        node.addAttribute("nextPeers", new ArrayList());
+        nodeLayer.addChild(node);
         }
-
+        
         for (int i = 0;i < NODES;i++) {
-            int n1 = i;
-            Linkable p = (Linkable) Network.get(i).getProtocol(1);
-            for (int j = 0; j < p.degree(); j++) {
-                int n2 = p.getNeighbor(j).getIndex();
-
-                PNode node1 = nodeLayer.getChild(n1);
-                PNode node2 = nodeLayer.getChild(n2);
-                Point2D start = node1.getFullBoundsReference().getCenter2D();
-                Point2D end = node2.getFullBoundsReference().getCenter2D();
-                ((ArrayList) node1.getAttribute("nextPeers")).add(node2);
-                ((ArrayList) node2.getAttribute("prevPeers")).add(node1);
-                PPath edge = new PPath(createArrow(new Point2D.Double(start.getX(), start.getY()),
-                        new Point2D.Double(end.getX(), end.getY())));
-
-
-                ((ArrayList) node1.getAttribute("edges")).add(edge);
-                ((ArrayList) node2.getAttribute("edges")).add(edge);
-                edge.addAttribute("nodes", new ArrayList());
-                ((ArrayList) edge.getAttribute("nodes")).add(node1);
-                ((ArrayList) edge.getAttribute("nodes")).add(node2);
-                edge.setVisible(false);
-                edgeLayer.addChild(edge);
-            }
+        int n1 = i;
+        Linkable p = (Linkable) Network.get(i).getProtocol(1);
+        for (int j = 0; j < p.degree(); j++) {
+        int n2 = p.getNeighbor(j).getIndex();
+        
+        PNode node1 = nodeLayer.getChild(n1);
+        PNode node2 = nodeLayer.getChild(n2);
+        Point2D start = node1.getFullBoundsReference().getCenter2D();
+        Point2D end = node2.getFullBoundsReference().getCenter2D();
+        ((ArrayList) node1.getAttribute("nextPeers")).add(node2);
+        ((ArrayList) node2.getAttribute("prevPeers")).add(node1);
+        PPath edge = new PPath(createArrow(new Point2D.Double(start.getX(), start.getY()),
+        new Point2D.Double(end.getX(), end.getY())));
+        
+        
+        ((ArrayList) node1.getAttribute("edges")).add(edge);
+        ((ArrayList) node2.getAttribute("edges")).add(edge);
+        edge.addAttribute("nodes", new ArrayList());
+        ((ArrayList) edge.getAttribute("nodes")).add(node1);
+        ((ArrayList) edge.getAttribute("nodes")).add(node2);
+        edge.setVisible(false);
+        edgeLayer.addChild(edge);
         }
+        }*/
 
         nodeLayer.addInputEventListener(
                 new PBasicInputEventHandler() {
@@ -103,64 +107,113 @@ public class ChordRing extends PFrame {
                         setEventFilter(filter);
                     }
 
+                    @Override
                     public void mouseEntered(PInputEvent e) {
                         super.mouseEntered(e);
                         if (e.getButton() == MouseEvent.NOBUTTON) {
+                            ArrayList fingers = (ArrayList) e.getPickedNode().getAttribute("fingers");
+                            PNode pred = nodeLayer.getChild(hashTable.get(
+                                    e.getPickedNode().getAttribute("predecessor")));
+                            PNode succ = nodeLayer.getChild(hashTable.get(
+                                    e.getPickedNode().getAttribute("successor")));
                             e.getPickedNode().setPaint(Color.GREEN);
                             e.getPickedNode().moveToFront();
+                            succ.moveToFront();
+                            pred.moveToFront();
+
+                            pred.setPaint(Color.RED);
+                            succ.setPaint(Color.BLUE);
+                            /*e.getPickedNode().moveToFront();
                             ArrayList prevPeers = (ArrayList) e.getPickedNode().getAttribute("prevPeers");
                             ArrayList nextPeers = (ArrayList) e.getPickedNode().getAttribute("nextPeers");
                             ArrayList edges = (ArrayList) e.getPickedNode().getAttribute("edges");
                             for (int i = 0; i < prevPeers.size(); i++) {
-                                PNode prevNode = (PNode) prevPeers.get(i);
-                                prevNode.setPaint(Color.RED);
-                                prevNode.moveToFront();
+                            PNode prevNode = (PNode) prevPeers.get(i);
+                            prevNode.setPaint(Color.RED);
+                            prevNode.moveToFront();
                             }
                             for (int i = 0; i < nextPeers.size(); i++) {
-                                PNode nextNode = (PNode) nextPeers.get(i);
-                                nextNode.setPaint(Color.BLUE);
-                                nextNode.moveToFront();
+                            PNode nextNode = (PNode) nextPeers.get(i);
+                            nextNode.setPaint(Color.BLUE);
+                            nextNode.moveToFront();
                             }
                             for (int i = 0; i < edges.size(); i++) {
-                                PPath edge = (PPath) edges.get(i);
-                                edge.setVisible(true);
-                            }
+                            PPath edge = (PPath) edges.get(i);
+                            edge.setVisible(true);
+                            }*/
                         }
                     }
 
+                    @Override
                     public void mouseExited(PInputEvent e) {
                         super.mouseExited(e);
                         if (e.getButton() == MouseEvent.NOBUTTON) {
+                            ArrayList fingers = (ArrayList) e.getPickedNode().getAttribute("fingers");
+                            PNode pred = (PNode) nodeLayer.getChild(hashTable.get(
+                                    e.getPickedNode().getAttribute("predecessor")));
+                            PNode succ = (PNode) nodeLayer.getChild(hashTable.get(
+                                    e.getPickedNode().getAttribute("successor")));
                             e.getPickedNode().setPaint(Color.WHITE);
                             e.getPickedNode().moveToBack();
+
+                            pred.setPaint(Color.WHITE);
+                            succ.setPaint(Color.WHITE);
+                            /*e.getPickedNode().moveToBack();
                             ArrayList prevPeers = (ArrayList) e.getPickedNode().getAttribute("prevPeers");
                             ArrayList nextPeers = (ArrayList) e.getPickedNode().getAttribute("nextPeers");
                             ArrayList edges = (ArrayList) e.getPickedNode().getAttribute("edges");
                             for (int i = 0; i < prevPeers.size(); i++) {
-                                PNode prevNode = (PNode) prevPeers.get(i);
-                                prevNode.setPaint(Color.WHITE);
-                                prevNode.moveToBack();
+                            PNode prevNode = (PNode) prevPeers.get(i);
+                            prevNode.setPaint(Color.WHITE);
+                            prevNode.moveToBack();
                             }
                             for (int i = 0; i < nextPeers.size(); i++) {
-                                PNode nextNode = (PNode) nextPeers.get(i);
-                                nextNode.setPaint(Color.WHITE);
-                                nextNode.moveToBack();
+                            PNode nextNode = (PNode) nextPeers.get(i);
+                            nextNode.setPaint(Color.WHITE);
+                            nextNode.moveToBack();
                             }
                             for (int i = 0; i < edges.size(); i++) {
-                                PPath edge = (PPath) edges.get(i);
-                                edge.setVisible(false);
-                            }
+                            PPath edge = (PPath) edges.get(i);
+                            edge.setVisible(false);
+                            }*/
                         }
                     }
                 });
     }
 
-    public void drawNodes(PLayer edgeLayer) {
+    private void drawNodes(PLayer nodeLayer) {
         for (int i = 0; i < NODES; i++) {
             ChordProtocol cp = (ChordProtocol) Network.get(i).getProtocol(0);
             double angle = getAngle(cp);
-            edgeLayer.addChild(createSingleNode(angle));
+            PPath node = nodePosition(angle);
+            hashTable.put(Network.get(i).getID(), i);
+            storeInfo(node, cp);
+            nodeLayer.addChild(node);
         }
+    }
+
+    private void storeInfo(PPath node, ChordProtocol cp) {
+        node.addAttribute("predecessor", cp.predecessor.getID());
+        node.addAttribute("successor", cp.successorList[0].getID());
+        node.addAttribute("fingers", new ArrayList());
+        int SIZE = cp.fingerTable.length;
+        for (int i = 0; i < SIZE; i++) {
+            ((ArrayList) node.getAttribute("fingers")).add(cp.fingerTable[i].getID());
+        }
+    }
+
+    private PPath nodePosition(double angle) {
+        float x = cx - 5 + (float) (radius * Math.sin(angle));
+        float y = cy - 5 - (float) (radius * Math.cos(angle));
+        return PPath.createEllipse(x, y, 10, 10);
+    }
+
+    private double getAngle(ChordProtocol cp) {
+        double chordId = cp.chordId.doubleValue();
+        double maxValue = Math.pow(2, cp.m);
+        double degree = ((chordId / maxValue) * 365);
+        double radian = degree / 180 * Math.PI;
+        return radian;
     }
 
     private Shape createArrow(Point2D start, Point2D end) {
@@ -189,35 +242,5 @@ public class ChordRing extends PFrame {
         path.lineTo(xe - dx2, ye - dy2);
 
         return path;
-    }
-
-    private ArrayList<Point2D> circlePoints(Point2D center, double r) {
-        double cx = center.getX();
-        double cy = center.getY();
-        double x, y, a;
-
-        ArrayList<Point2D> points = new ArrayList<Point2D>();
-        for (int i = 0; i < 360; i++) {
-            a = Math.toRadians(i);
-            x = cx + (r * Math.cos(a));
-            y = cy + (r * Math.sin(a));
-            points.add(new Point2D.Double(x, y));
-        }
-        return points;
-    }
-
-    private PPath createSingleNode(double angle) {
-        float x = cx - 5 + (float) (radius * Math.sin(angle));
-        float y = cy - 5 - (float) (radius * Math.cos(angle));
-        PPath node = PPath.createEllipse(x, y, 10, 10);
-        return node;
-    }
-
-    private double getAngle(ChordProtocol cp) {
-        double chordId = cp.chordId.doubleValue();
-        double maxValue = Math.pow(2, cp.m);
-        double degree = ((chordId / maxValue) * 365);
-        double radian = degree / 180 * Math.PI;
-        return radian;
     }
 }
