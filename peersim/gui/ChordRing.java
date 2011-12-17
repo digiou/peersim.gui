e410fce689208300d01fb62032e7e10dd196ddc5
digiou <cst06039@uop.gr>
2011-12-17 23:07:59 +0200

    basic tooltip

--- peersim/gui/ChordRing.java
+++ peersim/gui/ChordRing.java
@@ -9,14 +9,21 @@ import edu.umd.cs.piccolo.event.PInputEvent;
 import edu.umd.cs.piccolo.event.PInputEventFilter;
 import edu.umd.cs.piccolo.nodes.PPath;
 import edu.umd.cs.piccolox.PFrame;
+import edu.umd.cs.piccolox.pswing.PSwing;
 import edu.umd.cs.piccolox.pswing.PSwingCanvas;
 import edu.umd.cs.piccolox.util.PFixedWidthStroke;
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.event.InputEvent;
 import java.awt.event.MouseEvent;
+import java.awt.geom.Point2D;
 import java.util.ArrayList;
 import java.util.Hashtable;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextPane;
 import peersim.chord.ChordProtocol;
 import peersim.core.Network;
 
@@ -30,6 +37,7 @@ public class ChordRing extends PFrame {
     private static final int NODES = Network.size();
     private PCamera camera;
     int margin = 50;
+    int chordPosition;
     int width = SCREEN.width;
     int height = SCREEN.height;
     int radius = width / 2;
@@ -54,18 +62,30 @@ public class ChordRing extends PFrame {
         camera = canvas.getCamera();
         canvas.getRoot().addChild(edgeLayer);
         camera.addLayer(0, edgeLayer);
-
+        
+        findChordProtocol();
         drawNodes(nodeLayer);
         nodeLayer.addInputEventListener(new ChordMouseEventHandler());
+        nodeLayer.addInputEventListener(new TooltipHandler());
     }
 
     private Hashtable<Long, PNode> getRelationships() {
         return hashTable;
     }
+    
+    private void findChordProtocol(){
+        for(int i=0;i<Network.get(0).protocolSize();i++){
+            if(Network.get(0).getProtocol(i).getClass() == ChordProtocol.class){
+                chordPosition = i;
+                break;
+            }
+        }
+    }
 
     private void drawNodes(PLayer nodeLayer) {
         for (int i = 0; i < Network.size(); i++) {
-            ChordProtocol cp = (ChordProtocol) Network.get(i).getProtocol(0);
+            //ChordProtocol cp = (ChordProtocol) Network.get(i).getProtocol(0);
+            ChordProtocol cp = (ChordProtocol) Network.get(i).getProtocol(chordPosition);
             double angle = getAngle(cp);
             PPath node = nodePosition(angle);
             node.setStroke(new PFixedWidthStroke());
@@ -83,6 +103,7 @@ public class ChordRing extends PFrame {
         for (int i = 0; i < SIZE; i++) {
             ((ArrayList) node.getAttribute("fingers")).add(cp.fingerTable[i].getID());
         }
+        node.addAttribute("chordId", cp.chordId.toString());
     }
 
     private PPath nodePosition(double angle) {
@@ -108,6 +129,33 @@ public class ChordRing extends PFrame {
         line.setStroke(new PFixedWidthStroke());
         return line;
     }
+    
+    public String tooltipText(PNode aNode){
+        String tooltipText;
+        String chordId = (String) aNode.getAttribute("chordId");
+        tooltipText = "Node ID#: " + chordId + "\n";
+        /*String predChordId = (String) ((PNode) getRelationships().get(
+                (Long)aNode.getAttribute("predecessor"))
+                ).getAttribute("chordId");
+        tooltipText = tooltipText.concat("Pred ID#: " + predChordId + "\n");
+        String succChordId = (String) ((PNode) getRelationships().get(
+                (Long)aNode.getAttribute("successor"))
+                ).getAttribute("chordId");
+        tooltipText = tooltipText.concat("Succ ID#: " + succChordId + "\n");
+        ArrayList fingers = (ArrayList) aNode.getAttribute("fingers");
+        for(int i=0;i<fingers.size();i++){
+            String fingerId = (String) ((PNode) getRelationships().get(
+                (Long)fingers.get(i))
+                ).getAttribute("chordId");
+            if(i == fingers.size()-1){
+                tooltipText = tooltipText.concat("Finger " + i + " ID#: " + fingerId);
+            } else {
+                tooltipText = tooltipText.concat("Finger " + i + " ID#: " + fingerId + "\n");
+            }
+        }
+        System.out.println(tooltipText);*/
+        return tooltipText;
+    }
 
     public class ChordMouseEventHandler extends PBasicInputEventHandler {
 
@@ -130,6 +178,7 @@ public class ChordRing extends PFrame {
                 pred = (PNode) getRelationships().get((Long) e.getPickedNode().getAttribute("predecessor"));
                 succ = (PNode) getRelationships().get((Long) e.getPickedNode().getAttribute("successor"));
                 ArrayList fingerID = (ArrayList) e.getPickedNode().getAttribute("fingers");
+                System.out.println(fingerID.size());
                 fingerNodes = new ArrayList();
                 
                 lines.add(drawLine(e.getPickedNode(), pred));
@@ -143,9 +192,9 @@ public class ChordRing extends PFrame {
                 e.getPickedNode().setPaint(Color.GREEN);
                 pred.setPaint(Color.RED);
                 succ.setPaint(Color.BLUE);
-                e.getPickedNode().moveToFront();
                 succ.moveToFront();
                 pred.moveToFront();
+                e.getPickedNode().moveToFront();
                 edgeLayer.addChildren(lines);
             }
         }
@@ -159,6 +208,7 @@ public class ChordRing extends PFrame {
                     something = e.getPickedNode();
                     filter.setAcceptsMouseExited(false);
                     filter.setAcceptsMouseEntered(false);
+                    //e.getPickedNode().removeInputEventListener(this);
                 } else {
                     if(something.equals(e.getPickedNode())){
                         filter.setAcceptsMouseExited(true);
@@ -193,6 +243,49 @@ public class ChordRing extends PFrame {
                     lines.remove(0);
                 }
             }
+        }
+    }
+    
+    private class TooltipHandler extends PBasicInputEventHandler{
+        PSwing tooltip;
+        JPanel panel = new JPanel(new BorderLayout());
+        JTextArea textArea = new JTextArea();
+        JLabel label = new JLabel();
+        
+        public TooltipHandler(){
+            //textArea.setEditable(false);
+            //panel.add(textArea, BorderLayout.CENTER);
+            panel.add(label, BorderLayout.CENTER);
+            tooltip = new PSwing(panel);
+            tooltip.setVisible(false);
+            tooltip.setPickable(false);
+            nodeLayer.addChild(tooltip);
+        }
+        
+        @Override
+        public void mouseEntered(PInputEvent e){
+            PNode node = e.getPickedNode();
+            String text = tooltipText(node);
+            //textArea.setText(text);
+            label.setText(text);
+            tooltip.setVisible(true);
+            tooltip.moveToFront();
+        }
+        
+        @Override
+        public void mouseMoved(PInputEvent e){
+            if(tooltip.getVisible()){
+                PNode picked = e.getPickedNode();
+                Point2D point = e.getPositionRelativeTo(picked);
+                picked.localToParent(point);
+                point.setLocation(point.getX()+10, point.getY()+15);
+                tooltip.setOffset(point);
+            }
+        }
+        
+        @Override
+        public void mouseExited(PInputEvent e){
+             tooltip.setVisible(false);
         }
     }
 }
